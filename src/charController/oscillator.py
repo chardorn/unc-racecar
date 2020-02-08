9efb6c3fbabf80966f6f894f707c803dba16295e
shared account <otternes@cs.unc.edu>
2020-02-08 16:04:50 -0500

    simulator pre

--- src/charController/oscillator.py
+++ src/charController/oscillator.py
@@ -3,25 +3,26 @@ import rospy
 from ac_msgs.msg import drive_params
 from std_msgs.msg import Bool
 import time
+from race.msg import drive_param
 
 rospy.init_node("BugControl")
-DriveParamPublisher = rospy.Publisher("drive_parameters", drive_params, queue_size=10)
+DriveParamPublisher = rospy.Publisher("drive_parameters", drive_param, queue_size=10)
 EStopPublisher = rospy.Publisher("eStop", Bool, queue_size=10)
 
-msg = drive_params()
+msg = drive_param()
 while(1):
     #EStopPublisher.publish(False)
 
-    for a in range 100:
+    for a in range(100):
         time.sleep(0.01)
-        turn =  (a / 100)
+        turn =  (a / 100.0)
         msg.angle = turn
-        msg.velocity = 0
+        msg.velocity = 1
         DriveParamPublisher.publish(msg)
-    for b in range 100:
+    for b in range(100):
         time.sleep(0.01)
-        turn =  1 - (b / 100)
+        turn =  1 - (b / 100.0)
         msg.angle = turn
-        msg.velocity = 0
+        msg.velocity = 1
         DriveParamPublisher.publish(msg)
     continue
